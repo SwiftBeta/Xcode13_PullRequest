36483316d7b560965d02fda19fc7aacd8fbdc32f
SwiftBeta <swiftbeta.blog@gmail.com>
2021-06-14 16:58:47 +0200

    Initial Commit

---
+++ SwiftBetaXcode13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftBetaXcode13
+//
+//  Created by Home on 14/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftBetaXcode13/SwiftBetaXcode13App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBetaXcode13App.swift
+//  SwiftBetaXcode13
+//
+//  Created by Home on 14/6/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBetaXcode13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
